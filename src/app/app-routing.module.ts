293568023d0fdfab2216db88e960d65e754f332f
GaneshM <ganesh.masilamani@innovatiqconsulting.com>
2023-07-06 09:56:30 +0530

    course-list-table

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { canActivateChild } from './components/auth/auth.guard';
 import { LoginComponent } from './components/login/login.component';
 import { CourseListComponent } from './pages/course/List/course-list/course-list.component';
+import { CourseApprovalComponent } from './pages/course/course-approval/course-approval.component';
 
 const routes: Routes = [
   { path: '', component: CourseListComponent, canActivate: [canActivateChild] },
