76ab800f9f3039a95e5fab3dff3bf155f49f47e3
Vaishnavi Kaithepalli <vaishnavi@innovatiqconsulting.com>
2023-07-06 15:40:20 +0530

    course-list-2

--- src/app/pages/course/List/course-list2/course-list2.component.ts
+++ src/app/pages/course/List/course-list2/course-list2.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-course-list2',
@@ -6,5 +6,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./course-list2.component.css']
 })
 export class CourseList2Component {
+  @ViewChild("fileDropRef", { static: false })
+  fileDropEl!: ElementRef;
+  files: any[] = [];
+
+  /**
+   * on file drop handler
+   */
+  onFileDropped($event: any) {
+    this.prepareFilesList($event);
+  }
+
+  /**
+   * handle file from browsing
+   */
+  fileBrowseHandler(f: any) {
+    this.prepareFilesList(f.files);
+  }
+
+  /**
+   * Convert Files list to normal array list
+   * @param files (Files List)
+   */
+   prepareFilesList(files: Array<any>) {
+    for (const item of files) {
+      item.progress = 0;
+      this.files.push(item);
+    }
+    this.fileDropEl.nativeElement.value = "";
+  }
+  upload() {
+    document.getElementById('input')?.click();
+  }
+  back = false;
+  goBack() {
+    this.back = !this.back;
+  }
+
+  submit() {
+    console.log("submitted!");
+  }
 
 }
